638a2e6a2b20c70f61845369bf551e88425a5f13
Lkhan05 <m.elxan2005@gmail.com>
2025-11-12 17:51:53 +0400

    Spring boot learn

---
+++ Tests/Test/src/main/java/org/example/Main.java
@@ -0,0 +1,42 @@
+package org.example;
+
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println(abs(-5));
+//        System.out.println(abs(0));
+//        System.out.println(abs(5));
+
+        int[] arr1 = {1,2,3};
+        int[] arr2 = {4,5,6,7};
+//        System.out.println("Cemi: " + sumArray(arr1, arr2));
+        System.out.println(sumArray2(arr1) + sumArray2(arr2));
+    }
+
+    static int sumArray2(int [] arr){
+        if(arr == null || arr.length < 2) return 0;
+        int sum = 0;
+        Arrays.sort(arr);
+        for(int i = 1; i < arr.length-1; i++){
+            sum += arr[i];
+        }
+        return sum;
+    }
+
+    static int sumArray(int[] arr1, int[] arr2){
+        int sum = 0;
+        int i = 0;
+        while(i < arr1.length || i < arr2.length){
+            sum += (i < arr1.length) ? arr1[i] : 0;
+            sum += (i < arr2.length) ? arr2[i] : 0;
+            i++;
+        }
+        return sum;
+    }
+
+    public static int abs(int x){
+        return (x > 0) ? -x : x;
+    }
+}
