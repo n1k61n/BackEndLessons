3e3660fb2a7ac99c9450c69f0d977105d9e8b842
Lkhan05 <m.elxan2005@gmail.com>
2025-11-20 15:39:35 +0400

    web proekt-v3

--- NetBeans/mavenproject1/src/main/java/com/mycompany/mavenproject1/main.java
+++ NetBeans/mavenproject1/src/main/java/com/mycompany/mavenproject1/main.java
@@ -8,8 +8,7 @@ package com.mycompany.mavenproject1;
  *
  * @author mamed
  */
-public class Mavenproject1 {
-
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
