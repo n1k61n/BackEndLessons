d92dcffdc843ae1d5f49374478403b4f2af12336
Emin <90997900+n1k61n@users.noreply.github.com>
2025-12-15 12:32:29 +0400

    1

--- Tests/Test/src/main/java/org/example/Main.java
+++ Tests/Test/src/main/java/org/example/Main.java
@@ -1,38 +1,24 @@
 package org.example;
 
 
-import java.util.Arrays;
 
 public class Main {
-    final static String[] TOY_NAMES = {"Race Car", "Dollhouse", "Lego Set", "Puzzle Mat", "Robot Kit",
-            "Water Gun", "Toy Soldier", "Play Dough Tub", "Marble Run", "Kite",
-            "Frisbee", "Slime Container", "Drum Set", "Hula Hoop", "Skip Rope"};
 
-    final static String[] COLORS = {"Red", "Blue", "Yellow", "Green", "Pink", "Black", "White", "Purple"};
+    static void main(String[] args){
+        Integer [] numbers = {5, 0, -2, 7, 3};
+        printArray(numbers);
 
-    static void main(String[] args) {
-        String[] arrays = generateInsertStatements(100);
-        for(String sql : arrays) {
-            System.out.println(sql);
-        }
-    }
-
-    static String[] generateInsertStatements(int count) {
-        String[] statements = new String[count];
-        for (int i = 0; i < count; i++) {
-            String toyName = TOY_NAMES[(int) (Math.random() * TOY_NAMES.length)];
-            String color = COLORS[(int) (Math.random() * COLORS.length)];
-
-            double price = Math.random() * 100;
-            int stock = (int) (Math.random() * 250);
-            String sql = "INSERT INTO toys (toys_name, color, price, stock) VALUES ('%s', '%s', %.2f, %s),".formatted(toyName, color, price, stock);
-            statements[i] = sql;
-        }
-
-        return statements;
+        String[] words = {"hello", "world", "my", "dear", "friend"};
+        printArray(words);
     }
 
 
+    static <T>void  printArray(T[] arr){
+        for(T t : arr){
+            System.out.print(t + " ");
+        }
+        System.out.println();
+    }
 }
 
 
