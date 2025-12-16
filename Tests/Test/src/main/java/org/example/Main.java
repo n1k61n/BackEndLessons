44965070eeb1340b6856c14550abdd121d65bd5b
Emin <90997900+n1k61n@users.noreply.github.com>
2025-12-16 12:10:08 +0400

    solve leetcode problems

--- Tests/Test/src/main/java/org/example/Main.java
+++ Tests/Test/src/main/java/org/example/Main.java
@@ -1,24 +1,31 @@
 package org.example;
 
-
+import java.util.Arrays;
 
 public class Main {
 
     static void main(String[] args){
-        Integer [] numbers = {5, 0, -2, 7, 3};
-        printArray(numbers);
 
-        String[] words = {"hello", "world", "my", "dear", "friend"};
-        printArray(words);
+
+
+
+
     }
 
 
-    static <T>void  printArray(T[] arr){
-        for(T t : arr){
-            System.out.print(t + " ");
+
+    static  int addedInteger(int[] nums1, int[] nums2) {
+        if(nums1.length == 0 || nums2.length == 0){
+            return 0;
         }
-        System.out.println();
+
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        return nums2[0] - nums1[0];
+        
     }
+
 }
 
 
