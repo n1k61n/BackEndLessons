544b22a3003df6d49d757bd54300b07ae917f06a
Emin <90997900+n1k61n@users.noreply.github.com>
2026-01-01 09:40:15 +0400

    leetcode solve problem

--- Tests/src/main/java/org/example/Main.java
+++ Tests/src/main/java/org/example/Main.java
@@ -2,9 +2,45 @@ package org.example;
 
 public class Main {
     static void main(String[] args){
-      Student st1 = new Student("Emin", Level.ADVANCED);
-      Student st2 = new Student("Emin", Level.ADVANCED);
-      System.out.println(st2.equals(st1));
+
+        int[] nums = {2,0,2,1,1,0};
+        sortColors(nums);
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+
+    }
+
+// [2,0,2,1,1,0]
+    //[2,0,1]
+
+    public static void sortColors(int[] nums) {
+        int left = 0;
+        int right = nums.length-1;
+        int mid = 0;
+
+        while (mid <= right){
+            if(nums[mid] == 0){
+                swap(nums, left, mid);
+                left++;
+                mid++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else{
+                swap(nums, mid, right);
+                right--;
+            }
+        }
+    }
+
+
+
+    static void swap(int[] nums, int left, int right){
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
     }
 }
 
